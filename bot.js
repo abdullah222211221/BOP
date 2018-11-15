4ab17c081cec12ee9c94e4c7c4315026355dc2df
abdullah222211221 <44670477+abdullah222211221@users.noreply.github.com>
2018-11-15 21:20:42 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`**
+      حياك الله ي بعد راسي
+       خش بتنورنا فعاليات وكل شيء حلو موجود !
+        ي بعد عيني الرابط تحت
+         Spring SERVER
+
+                                 [ رابط سيرفرك ] **`)
+}).catch(console.error)
+})
+
+client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`**
+      حياك الله ي بعد راسي
+       خش بتنورنا فعاليات وكل شيء حلو موجود !
+        ي بعد عيني الرابط تحت
+         Spring SERVER
+
+                                 [ رابط سيرفرك ] **`)
+}).catch(console.error)
+})
+client.login('توكن البوت');
